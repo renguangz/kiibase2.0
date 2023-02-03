5f3a49b495256f9f072c9cb757c8032cd2343c31
renguangz <rg870328@gmail.com>
2023-02-03 15:40:52 +0800

    test: sidebar component

--- src/components/Sidebar/index.spec.tsx
+++ src/components/Sidebar/index.spec.tsx
@@ -1,12 +1,16 @@
-import { render } from "@testing-library/react";
-import { SidebarComponent } from ".";
+import { render } from '@testing-library/react';
+import { SidebarComponent } from '.';
 
-describe("Sidebar", () => {
+describe('Sidebar', () => {
   beforeEach(() => {
     render(<SidebarComponent />);
   });
 
-  describe("sidebar menu items", () => {
-    it.todo("");
+  describe('sidebar menu items', () => {
+    it.todo('should route to `/first-link` after clicking first button');
+    it.todo('should focus if route is `/first-link`');
+    it.todo('should expand if click second button');
+    it.todo('should expand if click on `sub1-1`');
+    it.todo('`second` and `sub1-1` button should focus after click `sub1-1-1`');
   });
 });
