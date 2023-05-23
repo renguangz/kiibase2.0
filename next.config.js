875f064f6d7551d33d8970d7a8e9d572e3cdb413
renguangz <rg870328@gmail.com>
2023-05-23 12:13:03 +0800

    feat: init

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/base',
   output: 'standalone',
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_API_HOST: process.env['NEXT_PUBLIC_API_HOST'],
-    NEXT_PUBLIC_API_STORAGE: process.env['NEXT_PUBLIC_API_STORAGE'],
-  },
   compiler: {
     styledComponents: true,
   },
