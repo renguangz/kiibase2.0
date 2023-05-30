250e5470264eeee8c2839d1c015c85cd81a2b6cc
renguangz <rg870328@gmail.com>
2023-05-30 11:32:24 +0800

    fix: update next config and environments file

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
-  API_HOST: process.env['API_HOST'] || 'http://localhost:2108/api',
-  STORAGE_HOST: process.env['STORAGE_HOST'] || 'http://localhost:2108/storage',
+  API_HOST: process.env['NEXT_PUBLIC_API_HOST'] || 'http://localhost:2108/api',
+  STORAGE_HOST: process.env['NEXT_PUBLIC_STORAGE_HOST'] || 'http://localhost:2108/storage',
   FRONTEND_HOST: 'https://localhost:3000',
 };
