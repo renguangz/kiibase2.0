a5888276aa782613f5fdf0f66c79014aa95606e3
renguangz <rg870328@gmail.com>
2023-06-30 16:19:01 +0800

    fix(TextareaField): add autoResize and rows config

--- src/components/FilterField/enhanceFilterField/TextareaField/index.tsx
+++ src/components/FilterField/enhanceFilterField/TextareaField/index.tsx
@@ -2,9 +2,11 @@ import { FieldProps } from '@/src/utils';
 import { Controller } from 'react-hook-form';
 import { InputTextarea } from 'primereact/inputtextarea';
 
-interface TextareaFieldProps extends FieldProps<string> {}
+interface TextareaFieldProps extends FieldProps<string> {
+  rows?: number;
+}
 
-export function TextareaField({ form, name, required, disabled, placeholder, width, height }: TextareaFieldProps) {
+export function TextareaField({ form, name, required, disabled, rows, placeholder, width }: TextareaFieldProps) {
   return (
     <div>
       <Controller
@@ -12,11 +14,13 @@ export function TextareaField({ form, name, required, disabled, placeholder, wid
         control={form.control}
         rules={{ required }}
         render={({ field }) => (
-          <div>
+          <div style={{ width: width ?? 'auto' }}>
             <InputTextarea
               role="dialog"
+              autoResize
+              rows={rows ?? 2}
               disabled={disabled}
-              style={{ width: '100%;' }}
+              style={{ width: '100%', height: '100%' }}
               id={field.name}
               value={field.value ?? ''}
               placeholder={placeholder ?? '請輸入'}
