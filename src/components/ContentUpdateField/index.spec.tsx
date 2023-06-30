be0e6955fa384b89ca33e06c08fe93160da29fa0
renguangz <rg870328@gmail.com>
2023-06-30 10:26:47 +0800

    test(ContentUpdateField): fix text for add title in required fields

--- src/components/ContentUpdateField/index.spec.tsx
+++ src/components/ContentUpdateField/index.spec.tsx
@@ -22,9 +22,9 @@ describe('ContentUpdateField', () => {
     it('should have five labels with expected labels', () => {
       setup();
       const labels = screen.queryAllByRole('heading').filter((label) => label.innerHTML !== '目前還沒有這個元件');
-      expect(labels).toHaveLength(5);
+      expect(labels).toHaveLength(10);
 
-      const expectedLabels = ['標題', '封面圖', '所在位置', '狀態', '權重'];
+      const expectedLabels = ['標題', '*', '封面圖', '*', '所在位置', '*', '狀態', '*', '權重', '*'];
       labels.forEach((label, index) => {
         expect(label.innerHTML).toBe(expectedLabels[index]);
       });
