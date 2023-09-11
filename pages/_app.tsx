a6dad43e29b098d01e619ec24735c75ba3314ae7
Martin Chen <martin@kiitzu.com>
2023-09-11 13:29:25 +0800

    refactor(_app): decoupling page's layout with _app.js
    解耦 page 頁面 layout 佈局綁定在 _app，由 page 元件 getLayout
    屬性自行決定佈局；若無宣告 getLayout 則使用 _app 中預設佈局

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/global.css';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+import '@/styles/global.css';
 import { SWRConfig } from 'swr';
 import { request } from '@/src/utils/request';
 import { LayoutProvider } from '@/src/layouts/layout/context/layoutcontext';
@@ -10,46 +12,32 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import '@/styles/layout/layout.scss';
 import '@/styles/demo/Demos.scss';
-import { useMemo } from 'react';
 import { useRouter } from 'next/router';
-import { AuthLayout } from '@/src/layouts/AuthLayout';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter();
+  const DefaultLayout = (page: ReactElement) => <LayoutProvider><Layout>{page}</Layout></LayoutProvider>
 
-  const authLayoutRoutes = useMemo(() => ['/auth/login', '/auth/signup'], []);
+  const getLayout = Component.getLayout ?? DefaultLayout;
 
-  const isAuthLayout = useMemo(
-    () => authLayoutRoutes.some((route) => router.asPath.includes(route)),
-    [authLayoutRoutes, router],
-  );
+  return <SWRConfig value={generateSWRConfig(router)}>{getLayout(<Component key={router.asPath} {...pageProps} />)}</SWRConfig>;
+}
 
-  return (
-    <SWRConfig
-      value={{
-        refreshInterval: 0,
-        shouldRetryOnError: false,
-        fetcher: request,
-        onError: (error) => {
-          if (error === 'Unauthorized') {
-            router.push('/auth/login');
-          } else {
-            router.push('/');
-          }
-        },
-      }}
-    >
-      {isAuthLayout ? (
-        <AuthLayout>
-          <Component {...pageProps} />
-        </AuthLayout>
-      ) : (
-        <LayoutProvider>
-          <Layout>
-            <Component key={router.asPath} {...pageProps} />
-          </Layout>
-        </LayoutProvider>
-      )}
-    </SWRConfig>
-  );
+function generateSWRConfig(router: ReturnType<typeof useRouter>) {
+  return {
+    refreshInterval: 0,
+    shouldRetryOnError: false,
+    fetcher: request,
+    onError: (error: any) => {
+      (error === 'Unauthorized') ? router.push('/auth/login') : router.push('/');
+    }
+  }
 }
