e863d4080fe327d70bdea75559f4cf12f3d093aa
Martin Chen <martin@kiitzu.com>
2023-09-13 17:11:59 +0800

    fix-24(typo): path alias typo

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^/(.*)$': '<rootDir>/$1',
-    '^@/(.*)$': '<rootDir>/$1'
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   testEnvironment: 'jest-environment-jsdom',
 };
