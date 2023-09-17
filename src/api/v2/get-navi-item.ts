1ab9a95a5a389742ea9b9006b5d5e19d7f878fed
Martin Chen <martin@kiitzu.com>
2023-09-17 18:34:48 +0800

    refactor-25(api): use .env to get JWT Cookie in get-navi-item api handler

--- src/api/v2/get-navi-item.ts
+++ src/api/v2/get-navi-item.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_API_TIMEOUT } from '/config/env';
+import { PUBLIC_API_TIMEOUT, PUBLIC_JWT_COOKIE_NAME } from '/config/env';
 import { endpointNaviItemGET } from './base/endpoint';
 import createFetchWithPlugins from '/src/utils/fetch';
 import Cookies from 'js-cookie';
@@ -9,7 +9,7 @@ export default function getNaviItem(callback?: FetchPluginOptions['callback']) {
     callback,
     headers: new Headers({
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${Cookies.get('token')}`,
+      Authorization: `Bearer ${Cookies.get(PUBLIC_JWT_COOKIE_NAME)}`,
     }),
   });
 
