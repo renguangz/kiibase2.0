7ad8898f66431761847f57167169e4915a79330e
Martin Chen <martin@kiitzu.com>
2023-09-17 18:33:09 +0800

    feat-25(env): add JWT Cookie Name on .env

--- config/env.ts
+++ config/env.ts
@@ -3,3 +3,4 @@ export const PUBLIC_SELF_API_HOST = process.env.NEXT_PUBLIC_SELF_API_HOST || 'ht
 export const PUBLIC_API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:2108/api';
 export const PUBLIC_STORAGE_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:2108/storage';
 export const PUBLIC_API_TIMEOUT = Number(process.env.NEXT_PUBLIC_API_TIMEOUT) || 5000;
+export const PUBLIC_JWT_COOKIE_NAME = process.env.NEXT_PUBLIC_JWT_COOKIE_NAME || 'token';
