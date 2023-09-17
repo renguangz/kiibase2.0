cd162a84fefc9a874ab6331ceac0694ebddb91f1
Martin Chen <martin@kiitzu.com>
2023-09-17 18:35:30 +0800

    feat-25(api): add GET /api/me API

--- src/api/v2/base/endpoint.ts
+++ src/api/v2/base/endpoint.ts
@@ -1,5 +1,9 @@
 import { PUBLIC_API_HOST } from '/config/env';
 
+export function endpointMeGET() {
+  return `${PUBLIC_API_HOST}/me`;
+}
+
 export function endpointNaviItemGET() {
   return `${PUBLIC_API_HOST}/naviItem`;
 }
