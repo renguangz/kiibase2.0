d51a33e9016f583a4469783dd7c3d8fdc0dc8e48
renguangz <rg870328@gmail.com>
2023-09-20 16:52:06 +0800

    feat: add home page and readme page

---
+++ pages/README/index.tsx
@@ -0,0 +1,3 @@
+export default function ReadmePage() {
+  return <div></div>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>TODO</h1>
+      <div>1. 新增一個 README Page 來說明 api 架構，包含可以使用哪些 component 以及他們的 config</div>
+      <div>
+        2. 新增 Select 選擇完去 call api 拿到其他資料 (role 頁面在選擇完 modules 之後去拿到所有的 api_functions)
+      </div>
+      <div>3. Logo 要換並且不要 route 到 `adminUser`</div>
+      <div>4. 重設密碼 api 實作(Backend)</div>
+      <Link href="/search-location">出發地、抵達地搜尋選單</Link>
+    </div>
+  );
+}
